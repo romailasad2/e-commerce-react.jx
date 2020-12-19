872be20ba8133ca56f154659ed50d394ae207e5f
Romail asad <romailasad2@gmail.com>
2020-12-20 00:38:42 +0500

    Added initial header in the app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
+
 import './App.css';
-import { Route } from 'react-router-dom';
+
+import { Route, Switch } from 'react-router-dom';
 import Homepage from './Components/Pages/homepage/homepage.component';
 import ShopPage from './Components/Pages/shop-page/shoppage.component';
-
+import Header from './Components/Components/header/header.component';
 
 class App extends Component{
   render(){
     return(
       <div>
+        <Header />
+        <Switch>
         <Route exact path='/' component={Homepage}/>
         <Route exact path='/shop' component={ShopPage}/>
+        </Switch>
       </div>
     )
   }
